cbc5ee7a24ce97c75f87b60ac590acf18512363d
Daniel Memije <d.memije26@gmail.com>
2018-03-27 12:44:11 -0600

    Correccion modelos

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,7 +1,7 @@
 class Quiz < ApplicationRecord
   belongs_to :quiz_type
   belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
-  belongs_to :user, :class_name => 'User', :foreign_key => 'evaluator_id'
+  belongs_to :evaluator, :class_name => 'User', :foreign_key => 'evaluator_id'
   has_many :user_answers
   has_one :simulation
 end
